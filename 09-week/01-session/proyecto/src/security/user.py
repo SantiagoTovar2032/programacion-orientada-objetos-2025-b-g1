b4bd53a7e4d75edd7ebd5a1e23c2cf191b6158ee
Ing. Tovar <stovar-2032b@corhuila.edu.co>
2025-10-03 19:49:28 -0500

    Update user.py

--- 09-week/01-session/proyecto/src/security/user.py
+++ 09-week/01-session/proyecto/src/security/user.py
@@ -9,3 +9,12 @@ class User:
 
     def __str__(self):
         return f"User: {self.username}, Person: [{self.person}]"
+        
+class Person(User):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"name: {self.name}, {super().__str__()}"
